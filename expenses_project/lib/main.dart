676942f154c4a8e8e7c646b31cb43b45202f8cb3
AbdelrhmanAmer <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>
2023-11-27 08:39:08 +0200

    Themes

--- expenses_project/lib/main.dart
+++ expenses_project/lib/main.dart
@@ -2,21 +2,40 @@ import 'package:expenses_project/widgets/expanses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
+  var myColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 59, 96, 167));
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData().copyWith(
+        colorScheme: myColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: myColorScheme.onPrimaryContainer,
+          foregroundColor: myColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: myColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: myColorScheme.primaryContainer
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: myColorScheme.onSecondaryContainer,
+            fontSize: 17
+          )
+        )
       ),
       home: const Expanses(),
     );
